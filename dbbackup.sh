823ac51240de4597a2fef44a1ce77ae47d83a494
Antima Mishra <55014206+antimalbn@users.noreply.github.com>
2023-02-02 17:15:13 +0530

    Update dbbackup.sh

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # -------------------------------------------------------------------------
  
-MyUSER="SET-MYSQL-USER-NAME"     # USERNAME
-MyPASS="SET-PASSWORD"       # PASSWORD 
-MyHOST="localhost"          # Hostname
  
 DEST="/backup"
  
  
 # Get data in dd-mm-yyyy format
 NOW="$(date +"%Y%m%d%H%M%S")"
-mysqldump timemanagement | gzip -9 > $DEST/timemanagement$NOW.sql.gz
+mysqldump --defaults-file=/root/.my.cnf timemanagement | gzip -9 > $DEST/timemanagement$NOW.sql.gz
