e46c7fc8de3ba4569ab6d7571367002715a17aeb
RoyFromServer <info@get2world.com>
2023-02-03 13:47:10 +0100

    test

--- dbbackup.sh
+++ dbbackup.sh
@@ -7,4 +7,10 @@ DEST="/backup"
  
 # Get data in dd-mm-yyyy format
 NOW="$(date +"%Y%m%d%H%M%S")"
-mysqldump --defaults-file=/root/.my.cnf timemanagement | gzip -9 > $DEST/timemanagement$NOW.sql.gz
+mysqldump --defaults-file=/root/.my.cnf --no-tablespaces dbZfdm | gzip -9 > $DEST/dbZfdm$NOW.sql.gz
+if [ $? -eq 0 ]
+then
+  echo "backup Successfully done"
+else
+  echo "backup successfully not done"
+fi
